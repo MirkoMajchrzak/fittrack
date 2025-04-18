e344c9d5032981d44389d14b483fc2ede0c5668b
MirkoMajchrzak <mirkomajchrzak@hotmail.com>
2025-04-18 17:24:37 +0200

    workaround dm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import useDarkMode from '../hooks/useDarkMode'
+// import useDarkMode from '../hooks/useDarkMode'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 
 function Header() {
-  const [darkMode, setDarkMode] = useDarkMode()
+  // const [darkMode, setDarkMode] = useDarkMode()
 
   return (
     <header className="p-4 bg-white dark:bg-gray-800 shadow-md flex justify-between items-center">
       <h1 className="text-xl font-bold text-black dark:text-white">FitTrack</h1>
       <Link to="/">🏠 Dashboard</Link>
-      <button
+      {/* <button
         onClick={() => setDarkMode(!darkMode)}
         className="text-sm bg-gray-200 dark:bg-gray-600 text-black dark:text-white px-3 py-1 rounded"
       >
         {darkMode ? '☀️ Light' : '🌙 Dark'}
-      </button>
+      </button> */}
     </header>
   )
 }
