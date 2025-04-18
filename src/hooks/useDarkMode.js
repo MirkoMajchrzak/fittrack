57b0b2e103efc17e64e2bbb81319cbdb3cba110c
MirkoMajchrzak <mirkomajchrzak@hotmail.com>
2025-04-18 17:38:17 +0200

    usedarkmode-off

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react'
 
 export default function useDarkMode() {
+  // Lade initialen Zustand aus localStorage
   const [darkMode, setDarkMode] = useState(() => {
     return localStorage.getItem('darkMode') === 'true'
   })
 
   useEffect(() => {
     const root = window.document.documentElement
-    if (darkMode) {
+
+    // 🧠 Nur Klasse setzen, wenn sie sich ändern muss
+    const hasDarkClass = root.classList.contains('dark')
+    if (darkMode && !hasDarkClass) {
       root.classList.add('dark')
-    } else {
+    } else if (!darkMode && hasDarkClass) {
       root.classList.remove('dark')
     }
+
+    // Speichern
     localStorage.setItem('darkMode', darkMode)
   }, [darkMode])
 
