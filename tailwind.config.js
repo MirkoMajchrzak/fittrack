458ff0d8c50319172725a7db269f83a2ec7cde92
MirkoMajchrzak <mirkomajchrzak@hotmail.com>
2025-04-18 16:58:24 +0200

    yet another dm fix try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class', // <--- wichtig!
+  darkMode: 'class', // 👈 ganz wichtig
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
